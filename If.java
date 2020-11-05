41a3037ec4af79d90cabfcc639b298108cb74e24
martamacias7e4 <marta.macias.7e4@itb.cat>
2020-11-05 11:24:48 +0100

    5/11/2020

---
+++ If.java
@@ -0,0 +1,10 @@
+public class If{
+	public static void main(String[] args){
+		boolean a = false;
+		if(a==true){ // si només és una línia no cal {...}
+			System.out.println("Hola");
+		}else{
+			System.out.println("Adeu");
+		}
+	}
+}
